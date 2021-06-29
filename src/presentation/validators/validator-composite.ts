f590fd2d31e02a198d0dbd337dbf5a20a1060a9b
Maycon <maycon.carlete@gmail.com>
2021-06-28 22:24:02 -0300

    feat: ensure ValidatorComposite throws when some validation fails

---
+++ src/presentation/validators/validator-composite.ts
@@ -0,0 +1,13 @@
+import { IValidate } from "../interfaces/validate";
+
+export class ValidatorComposite implements IValidate{
+    constructor(
+        private readonly validators: IValidate[]
+    ){}
+    validate(input: any): Error | undefined {
+        for (const validator of this.validators) {
+            validator.validate(input)
+        }
+        return
+    }
+}
