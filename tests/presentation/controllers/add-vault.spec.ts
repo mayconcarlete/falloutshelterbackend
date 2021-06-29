ab22d7723272dd0e49eaf32aad6b48afe181b673
Maycon <maycon.carlete@gmail.com>
2021-06-29 18:43:16 -0300

    test: ensure AddVaultController return 200 and a new Vault

--- tests/presentation/controllers/add-vault.spec.ts
+++ tests/presentation/controllers/add-vault.spec.ts
@@ -1,3 +1,5 @@
+import { VaultParams, Vault, EyeColor } from "../../../src/domain/models/vault"
+import { AddVault } from "../../../src/domain/usecases/add-vault"
 import { AddVaultController } from "../../../src/presentation/controllers/add-vault"
 import { IValidate } from "../../../src/presentation/interfaces/validate"
 import { THttpRequest } from "../../../src/presentation/types/http"
@@ -8,16 +10,35 @@ class MockValidator implements IValidate{
     }
 }
 
+const expected_response = {
+    id: 'valid_id',
+    name: 'Maycon',
+    age: 1,
+    eyeColor: EyeColor.GREEN,
+    hairColor: 'brown',
+}
+
+class MockAddVault implements AddVault{
+    create(vault: VaultParams): Promise<Vault> {
+        return new Promise((resolve, reject) => {
+            resolve(expected_response)
+        })
+    }
+}
+
 type SutTypes = {
     sut: AddVaultController
     validator: IValidate
+    addVaultUseCase: AddVault
 }
 
 const makeSut = ():SutTypes => {
     const validator = new MockValidator()
-    const sut  = new AddVaultController(validator)
-    return {sut, validator}
+    const addVaultUseCase = new MockAddVault()
+    const sut  = new AddVaultController(validator, addVaultUseCase)
+    return {sut, validator, addVaultUseCase}
 }
+
 describe('Add Vault Controller', () => {
     test('Should return bad request when validation fails', async () => {
         const {sut, validator} = makeSut()
@@ -34,7 +55,16 @@ describe('Add Vault Controller', () => {
     })
     test('Should return 200 when add vault with success', async () => {
         const {sut} = makeSut()
-        const response = await sut.handle({})
+        const request:THttpRequest = {
+            body:{
+                name: 'valid_name', 
+                age: 1, 
+                hairColor: 'valid_hair_color', 
+                eyeColor: 'valid_eye_color'
+            }
+        }
+        const response = await sut.handle(request)
         expect(response.statusCode).toBe(200)
+        expect(response.body).toEqual(expected_response)
     })
 })
