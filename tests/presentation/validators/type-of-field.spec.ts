5339248139131d38afacb618fb8c4a83e5fe8600
Maycon <maycon.carlete@gmail.com>
2021-06-28 21:57:12 -0300

    test: should return falsy when field has right type

--- tests/presentation/validators/type-of-field.spec.ts
+++ tests/presentation/validators/type-of-field.spec.ts
@@ -10,4 +10,14 @@ describe('TypeOfField Validator', () => {
         }
         expect(()=>sut.validate(body)).toThrow(TypeError)
     })
+    test('Should be falsy when type of field is right', () => {
+        const typeOfField = 'string'
+        const fieldName = 'any_field'
+        const sut = new TypeOfField(typeOfField, fieldName)
+        const body = {
+            [fieldName]: 'valid_type'
+        }
+        const response = sut.validate(body)
+        expect(response).toBeFalsy()
+    })
 })
