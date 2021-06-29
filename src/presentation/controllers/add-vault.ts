28886cf3fb8054e12c7e3513517a5933cdd4513c
Maycon <maycon.carlete@gmail.com>
2021-06-28 22:44:46 -0300

    feat: work in progress

--- src/presentation/controllers/add-vault.ts
+++ src/presentation/controllers/add-vault.ts
@@ -1,14 +1,30 @@
 import { IController } from "../interfaces/controller";
+import { IValidate } from "../interfaces/validate";
 import { THttpRequest, THttpResponse } from "../types/http";
 
 export class AddVaultController implements IController{
+    constructor(
+        private readonly validators: IValidate
+    ){}
     async handle(request: THttpRequest): Promise<THttpResponse> {
-        return new Promise((resolve, reject) => {
-            const response:THttpResponse = {
-                statusCode: 200,
-                body: 'any_value'
-            }
-            resolve(response)
-        })
+        try{
+            const body = request.body
+            this.validators.validate(body)
+            return new Promise((resolve, reject) => {
+                const response: THttpResponse = {
+                    statusCode: 200,
+                    body: 'hello world'
+                } 
+                resolve(response)
+            })
+        }catch(error){
+            return new Promise((resolve, reject) => {
+                const response: THttpResponse = {
+                    statusCode: 400,
+                    body: error
+                } 
+                resolve(response)
+            })
+        }
     }
 }
--- tests/presentation/controllers/add-vault.spec.ts
+++ tests/presentation/controllers/add-vault.spec.ts
@@ -1,13 +1,34 @@
 import { AddVaultController } from "../../../src/presentation/controllers/add-vault"
+import { IValidate } from "../../../src/presentation/interfaces/validate"
+import { THttpRequest } from "../../../src/presentation/types/http"
+
+class MockValidator implements IValidate{
+    validate(input: any): Error | undefined {
+        return
+    }
+}
 
 type SutTypes = {
     sut: AddVaultController
+    validator: IValidate
 }
+
 const makeSut = ():SutTypes => {
-    const sut  = new AddVaultController()
-    return {sut}
+    const validator = new MockValidator()
+    const sut  = new AddVaultController(validator)
+    return {sut, validator}
 }
 describe('Add Vault Controller', () => {
+    test('Should return bad request when validation fails', async () => {
+        const {sut} = makeSut()
+        const request:THttpRequest = {
+            body:{
+                'any_field': 'invalid_value'
+            }
+        }
+        const response = await sut.handle(request)
+        expect(response.statusCode).toBe(400)
+    })
     test('Should return 200 when add vault with success', async () => {
         const {sut} = makeSut()
         const response = await sut.handle({})
