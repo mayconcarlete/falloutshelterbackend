8060e07fae59e4fd9099b1548c0b7e3199f662d7
Maycon <maycon.carlete@gmail.com>
2021-06-30 12:49:36 -0300

    test: ensure TypeOfFields is called with correct params

--- tests/presentation/validators/type-of-field.spec.ts
+++ tests/presentation/validators/type-of-field.spec.ts
@@ -1,6 +1,17 @@
 import { TypeOfField } from "../../../src/presentation/validators/type-of-field"
 
 describe('TypeOfField Validator', () => {
+    test('Should call validate with correct params', () => {
+        const typeOfField = 'string'
+        const fieldName = 'any_field'
+        const sut = new TypeOfField(typeOfField, fieldName)
+        const body = {
+            [fieldName]: 'any_value'
+        }
+        const validateSpy = jest.spyOn(sut, 'validate')
+        sut.validate(body)
+        expect(validateSpy).toHaveBeenCalledWith(body)
+    })
     test('Should throw when type of field is wrong', () => {
         const typeOfField = 'number'
         const fieldName = 'any_field'
