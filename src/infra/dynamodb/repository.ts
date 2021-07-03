0b00a1f02c553d2778cdde07d1af10b75054bf02
Maycon <maycon.carlete@gmail.com>
2021-07-02 22:38:51 -0300

    feat: implement add dynamodb

--- src/infra/dynamodb/repository.ts
+++ src/infra/dynamodb/repository.ts
@@ -1,56 +1,55 @@
 import { AddVaultRepository } from "../../data/interfaces/vault/add-vault-repository";
 import { VaultParams, Vault } from "../../domain/models/vault";
+import {vaultTable} from './models/vault'
 import AWS from 'aws-sdk'
+import {v4} from 'uuid'
 
 export class DynamoDbRepository implements AddVaultRepository{
+    private aws
     constructor(){
+        const config = {
+            'endpoint': 'http://localhost:8000',
+            'region': 'us-east-1',
+            'accessKeyId': 'DUMMY_ID',
+            'secretAccessKey': 'DUMMY_KEY'
+        }
+        this.aws = new AWS.DynamoDB(config)    
+    }     
+    
+    async add(vault: VaultParams):Promise<Vault> {
+        const id = this.get_id() 
+        const params = this.mapObject({...vault, id})
+        
+        return new Promise((resolve, reject) => {
+            this.aws.putItem(params, (err, data) => {
+                if(err) reject(err)
+                resolve({...vault, id})
+            })
+        })
+    }
 
+    mapObject(vault:Vault){
+        return {
+            "TableName": 'Vault',
+            "ConditionExpression": "attribute_not_exists(id)",
+            "Item":{
+                "id": {"S": this.get_id()},
+                "age":{"S": vault.age},
+                "eyeColor": {"S": vault.eyeColor},
+                "name": {"S": vault.name},
+                "hairColor": {"S": vault.hairColor}
+            } 
+        }
+    }
+    get_id():string{
+        return v4()
     }
     async createTable(): Promise<void>{
-        try{
-            const config = {
-                'endpoint': 'http://localhost:8000',
-                'region': 'us-east-1',
-                'accessKeyId': 'DUMMY_ID',
-                'secretAccessKey': 'DUMMY_KEY',
-                'AWS_SDK_LOAD_CONFIG': 1
-            }
-            const vaultTable = {
-                TableName: 'Vault',
-                KeySchema:[
-                    {AttributeName: 'id', KeyType: 'HASH'},
-                    {AttributeName: 'name', KeyType: 'RANGE'}
-                ],
-                AttributeDefinitions:[
-                    {AttributeName: 'id', AttributeType: 'S'},
-                    {AttributeName: 'name', AttributeType: 'S'},
-                    {AttributeName: 'hairColor', AttributeType: 'S'},
-                    {AttributeName: 'eyeColor', AttributeType: 'S'},
-                    {AttributeName: 'age', AttributeType: 'S'}
-                ]
-            }
-            const dynamoDb = new AWS.DynamoDB(config)
-            const creatingTable = await dynamoDb.createTable(vaultTable).promise()
-            console.log('promise criada' + creatingTable)
-        }catch(e){
-            console.log('Error')
-            console.log(e)
-        }
-    }
-
-    async add(vault: VaultParams): Promise<Vault> {
-        const addedVault = {
-            id: 'valid_id',
-            age:1,
-            eyeColor: 'BROWN',
-            name: 'MAYCON',
-            hairColor: 'BROWN'
-        } 
-        const config = {
-
-        }
-        return new Promise((resolve, reject) => {
-            resolve(addedVault)
-        })
+            return new Promise((resolve, reject) => {
+                this.aws.createTable(vaultTable, function(err, data){
+                    if(err) reject(err)
+                    resolve()
+                })
+            })
     }
 }
