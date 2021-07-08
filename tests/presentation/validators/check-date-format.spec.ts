426170527c3bc38bbdbd6494c3e9c009d5568694
Maycon <maycon.carlete@gmail.com>
2021-07-08 13:24:09 -0300

    test: ensure CheckDateFormat be falsy when validate succeeds

--- tests/presentation/validators/check-date-format.spec.ts
+++ tests/presentation/validators/check-date-format.spec.ts
@@ -1,14 +1,33 @@
 import { CheckDateFormatError } from "../../../src/presentation/errors/check-date-format"
+import { IValidate } from "../../../src/presentation/interfaces/validate"
 import { CheckDateFormat } from "../../../src/presentation/validators/check-date-format"
+
+type SutTypes = {
+    sut: IValidate
+}
+
+const fieldName = 'date'
+
+const makeSut = ():SutTypes => {
+    const sut = new CheckDateFormat(fieldName)
+    return{sut}
+}
 
 describe('Check Date Format', () => {
     test('Should return error if Date is in wrong format', () => {
-        const fieldName = 'date'
-        const sut = new CheckDateFormat(fieldName)
+        const {sut} = makeSut()
         const body = {
             [fieldName]: 'invalid_date_format'
         } 
         const validateDate = sut.validate(body)
         expect(validateDate).toEqual(new CheckDateFormatError())
+    })
+    test('Should be falsy when validation succeeds', () => {
+        const {sut} = makeSut()
+        const body = {
+            [fieldName]: '2020-06-06'
+        }
+        const validateDate = sut.validate(body)
+        expect(validateDate).toBeFalsy()
     })
 })
