01f72dde71a401dab5771a44f413e8493cfccadb
Maycon <maycon.carlete@gmail.com>
2021-07-11 18:46:28 -0300

    test: change remove undefined params strategy

--- tests/data/helpers/remove-undefined-params.spec.ts
+++ tests/data/helpers/remove-undefined-params.spec.ts
@@ -2,7 +2,8 @@ import { RemoveUndefinedParams } from '../../../src/data/helpers/remove-undefine
 
 describe('Remove undefined Params', () => {
   test('Should call remove with correct params', () => {
-    const sut = new RemoveUndefinedParams()
+    const fields = ['age', 'hairColor', 'eyeColor', 'name']
+    const sut = new RemoveUndefinedParams(fields)
     const vaultParams = {
       param0: null,
       param1: 'valid_value',
@@ -14,7 +15,8 @@ describe('Remove undefined Params', () => {
     expect(removeSpy).toHaveBeenCalledWith(vaultParams)
   })
   test('should remove undefined or null params to query', () => {
-    const sut = new RemoveUndefinedParams()
+    const fields = ['age', 'hairColor', 'eyeColor', 'name']
+    const sut = new RemoveUndefinedParams(fields)
     const vaultParams = {
       param0: null,
       param1: 'valid_value',
@@ -22,9 +24,21 @@ describe('Remove undefined Params', () => {
       param3: undefined
     }
     const response = sut.remove(vaultParams)
+    expect(response).toEqual({})
+  })
+  test('should remove undefined or null params to query and let valid values', () => {
+    const fields = ['age', 'hairColor', 'eyeColor', 'name']
+    const sut = new RemoveUndefinedParams(fields)
+    const vaultParams = {
+      age: null,
+      hairColor: 'valid_value',
+      eyeColor: 'valid_value',
+      name: undefined
+    }
+    const response = sut.remove(vaultParams)
     expect(response).toEqual({
-      param1: 'valid_value',
-      param2: 'valid_value'
+      hairColor: 'valid_value',
+      eyeColor: 'valid_value'
     })
   })
 })
