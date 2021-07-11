957ed9a86d53a6beae61fd0b81dd4f132ceb6b80
Maycon <maycon.carlete@gmail.com>
2021-07-11 19:05:49 -0300

    feat: add query-vault router to server

--- src/main/server.ts
+++ src/main/server.ts
@@ -1,11 +1,11 @@
 import express from 'express'
 import { addVault } from './routes/add-vault'
 import { getVaultByIdRoute } from './routes/get-vault-by-id'
-
+import {queryVault} from './routes/query-vault'
 const app = express()
 app.use(express.json())
 
 getVaultByIdRoute(app)
 addVault(app)
-
+queryVault(app)
 export default app
