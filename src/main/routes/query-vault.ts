adb6e65cd13924c8ddbda14c1173063bbaca5c1e
Maycon <maycon.carlete@gmail.com>
2021-07-14 07:36:30 -0300

    feat: add updateAgeDecorator to query vault

--- src/main/routes/query-vault.ts
+++ src/main/routes/query-vault.ts
@@ -1,8 +1,10 @@
 import { Express } from 'express'
 import { adapterController } from '../adapters/controller'
 import { makeQueryVaultController } from '../factories/controllers/query-vault/make-query-vault'
+import { makeUpdateAgeDecorator } from '../factories/decorators/update-age-vault'
 
 export const queryVault = (app: Express): void => {
   const makeQueryController = makeQueryVaultController()
-  app.post('/query-vault', adapterController(makeQueryController))
+  const updateAgeDecorator = makeUpdateAgeDecorator(makeQueryController)
+  app.post('/query-vault', adapterController(updateAgeDecorator))
 }
