d01168977b38d014e2aec42460211490f9e9c116
Maycon <maycon.carlete@gmail.com>
2021-07-14 21:03:14 -0300

    test: ensure GetTime throws error when getTimeRepository throws

---
+++ tests/data/usecases/get-time/get-time.spec.ts
@@ -0,0 +1,30 @@
+import { GetTimeRepository } from "../../../../src/data/interfaces/time/get-time"
+import { GetTimeUseCase } from "../../../../src/data/usecases/get-time"
+import { MockGetTimeRepository } from "./mocks/get-time-repository"
+
+type SutTypes ={
+    sut: GetTimeUseCase
+    getTimeRepository: GetTimeRepository
+}
+
+const makeSut = ():SutTypes => {
+    const getTimeRepository = new MockGetTimeRepository()
+    const sut = new GetTimeUseCase(getTimeRepository)
+    return {sut, getTimeRepository}
+}
+
+describe('GetTimeUseCase class', () => {
+    test('Should throw if getTimeRepository throws', async () => {
+        const {sut, getTimeRepository} = makeSut()
+        jest.spyOn(getTimeRepository, 'get').mockImplementationOnce(async () => {
+            return new Promise((resolve, reject) => {
+                throw new Error()
+            })
+        })
+        try{
+            const time = await sut.getTime()
+        }catch(error){
+            expect(error).toEqual(new Error())
+        }
+    })
+})
