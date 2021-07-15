325809771fea363b394ebe07fc39312b394ccdee
Maycon <maycon.carlete@gmail.com>
2021-07-15 08:21:46 -0300

    feat: ensure VaultRepository.get returns a vault when id exists in db

--- tests/infra/mongodb/vault-repository/vault-repository.spec.ts
+++ tests/infra/mongodb/vault-repository/vault-repository.spec.ts
@@ -1,6 +1,7 @@
 import { MongoDB } from "../../../../src/infra/mongodb/helper"
 import VaultRepositoryModel from "../../../../src/infra/mongodb/models/vault"
 import { VaultRepository } from "../../../../src/infra/mongodb/vault-repository"
+import { mockedVault } from "./mocks/vault"
 
 const makeSut = ():VaultRepository =>{
     return new VaultRepository()
@@ -14,10 +15,20 @@ describe('VaultRepository class', () => {
     beforeEach(async () => {
         VaultRepositoryModel.deleteMany()
     })
-    test('should throw when get method cant find vault by id', async() => {
+    test('should return null when get method cant find vault by id', async() => {
         const sut = makeSut()
         const id = 'invalid_id'
         const vault = await sut.get(id)
         expect(vault).toBe(null)
+    })
+    test('Should return a vault when get method find a vault by id', async() => {
+        const sut = makeSut()
+        const addVault = await VaultRepositoryModel.create(mockedVault) 
+        const vault = await sut.get(addVault.id)
+        expect(vault!.id).toBeTruthy()
+        expect(vault!.name).toBe('MAYCON CARLETE')
+        expect(vault!.eyeColor).toBe('BROWN')
+        expect(vault!.hairColor).toBe('BROWN')
+        expect(vault!.age).toBe('1990-07-16')
     })
 })
