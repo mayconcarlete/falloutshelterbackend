c97ca5562be9addd9889e12c0d6d6e4659bb2997
Maycon <maycon.carlete@gmail.com>
2021-07-17 11:04:47 -0300

    feat: add disconnect method to mongo base class

--- src/infra/mongodb/helper.ts
+++ src/infra/mongodb/helper.ts
@@ -4,12 +4,16 @@ export class MongoDB {
   private mongooseConnection: undefined|Mongoose
 
   async connect (): Promise<Mongoose> {
-    this.mongooseConnection = await mongoose.connect('mongodb://0.0.0.0:27017/teste',{
+    this.mongooseConnection = await mongoose.connect('mongodb://mongo:27017/teste',{
       useCreateIndex: true,
       useNewUrlParser: true,
       useUnifiedTopology: true
     })
     return this.mongooseConnection
+  }
+
+  async disconnect (): Promise<void> {
+    await this.mongooseConnection?.disconnect()
   }
 
   static mapObject (object: any): any {
