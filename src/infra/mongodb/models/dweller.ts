0c9e68645dbefcb6cb8f8f33b80811857cedc58d
Maycon <maycon.carlete@gmail.com>
2021-07-18 18:53:44 -0300

    refactor: rename vaultSchema to dwellerSchema

--- src/infra/mongodb/models/dweller.ts
+++ src/infra/mongodb/models/dweller.ts
@@ -7,13 +7,13 @@ export interface DwellerInterface extends Document {
   eyeColor: string
 }
 
-const VaultSchema: Schema = new Schema({
+const DwellerSchema: Schema = new Schema({
   name: { type: String, required: true },
   age: { type: String, required: true },
   hairColor: { type: String, required: true },
   eyeColor: { type: String, required: true }
 })
 
-const DwellerRepositoryModel = mongoose.model<DwellerInterface>('Vault', VaultSchema)
+const DwellerRepositoryModel = mongoose.model<DwellerInterface>('Dweller', DwellerSchema)
 
 export default DwellerRepositoryModel
