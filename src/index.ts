625d2ce789b74782d93f527a5f4b2cc3a03d5914
Maycon <maycon.carlete@gmail.com>
2021-08-09 21:21:45 -0300

    test: new way to pass environment variables

--- src/index.ts
+++ src/index.ts
@@ -6,9 +6,11 @@ const PORT = 3000
 
 mongoRepository.connect().then(async () =>
   server.listen(PORT,() => {
+    console.log(`Tudo ok: ${process.env.MONGO_URL}`)
     console.log(`We are online on port: ${PORT}`)
   })
 ).catch(error => {
   console.log('Error')
+  console.log(`Errrouu: ${process.env.MONGO_URL}`)
   console.log(error)
 })
